2c5482655197ac8ad9f795c4a881acd8bf8beb26
Renato Vasquez <renatovasquez@Renatos-MacBook-Pro.local>
2023-07-21 15:21:59 +0200

    add initial workflow

---
+++ src/test/java/com/nato/demogithubactions/DemoGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nato.demogithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
